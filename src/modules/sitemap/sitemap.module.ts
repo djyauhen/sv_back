1d9e54889d747f3706741175c5d60d8d5f1c0ae9
Djyauhen <fursaevgenij@gmail.com>
2025-05-11 03:52:04 +0300

    final all2

--- src/modules/sitemap/sitemap.module.ts
+++ src/modules/sitemap/sitemap.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {SitemapController} from "./sitemap.controller";
+import {ArticlesService} from "../articles/articles.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Users} from "../user/entities/user.entity";
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Users])],
     controllers: [SitemapController],
+    providers: [ArticlesService],
 })
-export class SitemapModule {}
+export class SitemapModule {
+}
